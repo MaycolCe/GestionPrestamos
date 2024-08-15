36130a0bb13d0318482f590d8bf3f974bd60b892
Maycol <maycolcetina@hotmail.com>
2024-08-14 21:57:16 -0500

    prueba ignore

--- GestionPrestamos.DataAccess/obj/Debug/net8.0/GestionPrestamos.DataAccess.AssemblyInfo.cs
+++ GestionPrestamos.DataAccess/obj/Debug/net8.0/GestionPrestamos.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionPrestamos.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e2055c647392bae8b022240134e3d368480f384")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5224aeee36818dcff9c7336f1f2014f8582e4999")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionPrestamos.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionPrestamos.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
